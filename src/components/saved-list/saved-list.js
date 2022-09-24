684efc6fe67cb09bae684c7232c3a17630ce42fb
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-24 10:43:36 -0600

    2 new components
    2 new components Calendar-list and Saved-list

---
+++ src/components/saved-list/saved-list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './saved-list.css'
+
+const SavedEvents = () => {
+	return (
+		<div id='saved-list'>
+
+			<h3 id='saved-title'>
+
+				<div id='saved-content'>
+
+					<p id='saved-items'>
+
+					</p>
+
+				</div>
+
+			</h3>
+
+		</div>
+	)
+}
+
+export default SavedEvents
