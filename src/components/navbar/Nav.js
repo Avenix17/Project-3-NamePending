3618b310a2450c2b7cb4d83a02ee66a7c22db1b1
Chef <kvu6111@gmail.com>
2022-09-24 10:46:11 -0600

    updated data

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -6,25 +6,23 @@ import './nav.css';
 const Navbar = () => {
     return (
         <nav className = 'nav'>
-            <div className = 'nav-content'>
-                <a className = "nav-items">
-                    
-                    {/* Saved Events */}
-                    <a href = 'SavedEvents'>
-                            Saved Events
-                    </a>
-
-                    {/* Search Bar */}
-                    <a>
-                        This will be the Search Bar
-                        <Searchbar />
-                    </a>
-
-                    {/* Logout */}
-                    <a>
-                        Logout Button
-                    </a>
+            <div className = 'nav-items'>
+                
+                {/* Saved Events */}
+                <a href = '#SavedEvents'>
+                        Saved Events
                 </a>
+
+                {/* Search Bar */}
+                <div id = 'searchBar'>
+                    This will be the Search Bar
+                    <Searchbar />
+                </div>
+
+                {/* Logout */}
+                <button id = 'logout'>
+                    Logout Button
+                </button>
             </div>
         </nav> 
     )
--- src/components/navbar/Searchbar.js
+++ src/components/navbar/Searchbar.js
@@ -2,9 +2,50 @@ import React, {useState} from 'react';
 import "../../App.css";
 
 const Searchbar = () => {
-    const [query, setQuery] = useState("")
+
+    const [searchInput, setSearchInput] = useState("");
+
+    // Handle input changes
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+    };
+
+    // Use search input to go through database info
+    // if (searchInput.length > 0) {
+    //     countries.filter(( * database here * ) => {
+    //         return { * database here * }.name.match(searchInput)
+    //     })
+    // };
+
     return (
         <div className = 'searchBar'>
+
+            {/* Handle user input here */}
+            <input
+                type = 'text'
+                placeholder = 'Search here'
+                onChange = {handleChange}
+                value = {searchInput} 
+                />
+
+            {/* <table>
+            <tr>
+
+            </tr>
+
+            {{* Database here *}.map(( {* Database here *} , index) => {
+
+
+
+            <tr>
+                <td>{{* Database here *} {* Array data name *} }</td>
+                <td>{{* Database here *} {* Array data name *} }</td>
+
+            </tr>
+
+            })}
+            </table> */}
 
         </div>
     ) 
