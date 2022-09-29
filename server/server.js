dc84b5ef0df79f30ff11907e579736136d7677a0
Chef <kvu6111@gmail.com>
2022-09-29 16:44:48 -0600

    connected backend to graphql

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const express = require('express');
-// const { cwd } = require('process');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require ('./routes');
 
 const app = express();
 const PORT = process.env.port || 3001;
+const server = new ApolloServer({
+  typeDefs, resolvers
+})
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(routes);
 
-db.once("open", () => {
+const startApolloServer = async (typeDefs, resolvers) => {
+await server.start();
+server.applyMiddleware({ app });
+
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-  });
-});
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
+})
+};
+
+startApolloServer(typeDefs, resolvers);
