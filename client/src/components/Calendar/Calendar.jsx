66ffd12c0743314efe856c155ee2e793408a7bcf
Chef <kvu6111@gmail.com>
2022-09-30 17:33:11 -0600

    graphql frontend errors in calendar list

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -5,8 +5,13 @@ import './calendar.css';
 import 'react-calendar/dist/Calendar.css';
 import tack from '../../assets/tack.png';
 
+import { QUERY_EVENTS } from '../../utils/queries';
+import { useQuery } from '@apollo/client';
+
 function Calendarfun() {
   const [date, setDate] = useState(new Date());
+  const { data } = useQuery(QUERY_EVENTS);
+  const events = data?.events || [];
 
   return (
     <div className='calendar'>
@@ -18,7 +23,8 @@ function Calendarfun() {
         {/* Frontend peeps: Selected date is where we will want the calendar-list (event date) to appear */}
         <img className='tack'  src={tack} alt="It's a tack!" />
         <h3 className='selected-date'>{date.toDateString()}</h3>
-        <EventDate />
+        <EventDate 
+          events = {events} />
       </div>
     </div>
   );
--- client/src/components/calendar-list/calendar-list.jsx
+++ client/src/components/calendar-list/calendar-list.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import './calendar-list.css'
 
-const EventDate = () => {
+const EventDate = (events) => {
 	return (
 		<div id = 'calendar-list'>
 			Calendar List
 
 		<div id = 'date'>
-
 			<div id = 'list-items'>
-
+			{events &&
+				events.map((event) => (
+				<div key={event._id}>
+					<h4>
+					{event.username} <br />
+					<span>
+						had this event on {event.startdate}
+					</span>
+					</h4>
+					<p>{event.description}</p>
+					Join the discussion on this event.
+				</div>
+				))}
 			</div>
-
 		</div>
-
 		</div>
-		
-		
-		
-		
 		)
 };
 
