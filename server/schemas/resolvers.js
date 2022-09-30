a5d97b5846526d6d38e708d115a33ae181d1d22b
kate patch <kate.epatch@gmail.com>
2022-09-29 18:48:15 -0600

    resolvers through koi

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,143 +1,41 @@
-const { AuthenticationError } = require('apollo-server-express');
+const { GraphQLScalarType, Kind } = require('graphql');
 const { Users, Events, Comments } = require('../models');
-const { signToken } = require('../utils/auth');
-
-
-const resolvers = {
-  Query: {
-    categories: async () => {
-      return await Category.find();
-    },
-    products: async (parent, { category, name }) => {
-      const params = {};
-
-      if (category) {
-        params.category = category;
-      }
-
-      if (name) {
-        params.name = {
-          $regex: name
-        };
-      }
-
-      return await Product.find(params).populate('category');
-    },
-    product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
-    },
-    user: async (parent, args, context) => {
-      if (context.user) {
-        const user = await Users.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'events'
-        });
-
-        user.events.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
-        return user;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    event: async (parent, { _id }, context) => {
-      if (context.user) {
-        const user = await Users.findById(context.user._id).populate({
-          path: 'orders.products',
-          populate: 'category'
-        });
-
-        return user.events.id(_id);
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ products: args.products });
-      const line_items = [];
-
-      const { products } = await order.populate('products');
-
-      for (let i = 0; i < products.length; i++) {
-        const product = await stripe.products.create({
-          name: products[i].name,
-          description: products[i].description,
-          images: [`${url}/images/${products[i].image}`]
-        });
-
-        const price = await stripe.prices.create({
-          product: product.id,
-          unit_amount: products[i].price * 100,
-          currency: 'usd',
-        });
-
-        line_items.push({
-          price: price.id,
-          quantity: 1
-        });
-      }
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`
-      });
-
-      return { session: session.id };
-    }
-  },
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await Users.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    addOrder: async (parent, { products }, context) => {
-      console.log(context);
-      if (context.user) {
-        const order = new Order({ products });
-
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
-
-        return order;
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        return await Users.findByIdAndUpdate(context.user._id, args, { new: true });
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    updateProduct: async (parent, { _id, quantity }) => {
-      const decrement = Math.abs(quantity) * -1;
-
-      return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
-    },
-    login: async (parent, { email, password }) => {
-      const user = await Users.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    }
-  }
+const dateScalar = {
+    Date: new GraphQLScalarType({
+        name: 'Date',
+        description: 'Date custom scalar type',
+        parseValue(value) {
+        return new Date(value) // value from the client
+        },
+        serialize(value) {
+        return value.getTime() // value sent to the client
+        },
+        parseLiteral(ast) {
+        if (ast.kind === Kind.INT) {
+            return new Date(+ast.value) // ast value is always in string format
+        }
+        return null
+        }
+    })
+}
+const resolver = {
+    Date: dateScalar,
+        Query: {
+            getAllUsers: async () => {
+                return Users.find({})
+            },
+            getAllEvents: async () => {
+                return Events.find({})
+            },
+            getAllComments: async () => {
+                return Comments.find({})
+            }
+        },
+        Mutation: {
+            createUser(parent, args) {
+                const userCreation = Users.create(args);
+                return userCreation;
+            },
+        },
 };
-
-module.exports = resolvers;
+module.exports =  resolver;
