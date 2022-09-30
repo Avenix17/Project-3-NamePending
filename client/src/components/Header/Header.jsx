0f9ac143bcfb51e556043826ed4ff7b33ac17389
Avyrie Fellows <avyriefellows@gmail.com>
2022-09-30 15:00:22 -0600

    Finished css for nav and header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./header.css";
-import Navigation from '../navbar/Nav';
-// import myLogo from 
+import Navigation from "../navbar/Nav";
+import tack from "../../assets/tack.png";
 
 //passing in props to generate Header
 const Header = (props) => {
-	console.log("Head props", props)
-	const { currentNav, setCurrentNav, setUser } = props;
-	return (
-			<header id="header">
-				<div id = "head-content">
-					<h1>Under A-Tack</h1>
-					<Navigation currentNav={currentNav} setCurrentNav={setCurrentNav} setUser={setUser} />
-				</div>
-			</header>
-	)
-
-}
-export default Header
+  console.log("Head props", props);
+  const { currentNav, setCurrentNav, setUser } = props;
+  return (
+    <header id="header">
+      <div id="head-content">
+        <div className="tack-title">
+          <h1>Under A-Tack</h1>
+          <img className="tack" src={tack} alt="It's a tack!" />
+        </div>
+        <Navigation
+          currentNav={currentNav}
+          setCurrentNav={setCurrentNav}
+          setUser={setUser}
+        />
+      </div>
+    </header>
+  );
+};
+export default Header;
