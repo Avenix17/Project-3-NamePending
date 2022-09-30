2ff31b3a173a8d78ac24bac63138665906fc1328
Avyrie Fellows <avyriefellows@gmail.com>
2022-09-30 14:19:55 -0600

    Commented apollo stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,50 @@
+import React, { useState } from "react";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Calendarfun from "./components/Calendar/Calendar";
 import EventInput from "./components/create-event/CreateEvent";
 import SavedEvents from "./components/saved-list/saved-list";
-import React, { useState } from "react";
 import Login from "./components/login/Login";
 import "./App.css";
+
+// Construct our main GraphQL API endpoint
+// const httpLink = createHttpLink({
+//   uri: "/graphql",
+// });
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem("id_token");
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   const [nav] = useState(["home", "createEvent"]);
   const [currentNav, setCurrentNav] = useState(nav[0]);
   const [user, setUser] = useState();
 
+  // Switch function for navbar, home button leads to homepage and createEvent button leads to Create Event
   function renderComponent(currentNav) {
     switch (currentNav) {
       case "home":
@@ -40,18 +73,20 @@ function App() {
   }
 
   return (
-    <div className="screen">
-      <Header
-        title="Under a-tack"
-        currentNav={currentNav}
-        setCurrentNav={setCurrentNav}
-        setUser={setUser}
-      />
+    // <ApolloProvider client={client}>
+      <div className="screen">
+        <Header
+          title="Under a-tack"
+          currentNav={currentNav}
+          setCurrentNav={setCurrentNav}
+          setUser={setUser}
+        />
 
-      <main id="main">{mainComponent}</main>
+        <main id="main">{ mainComponent }</main>
 
-      <Footer id="footer" version="V0.1.1----" />
-    </div>
+        <Footer id="footer" version="V0.1.1----" />
+      </div>
+    // </ApolloProvider>
   );
 }
 
