fb3b2b7a5811f380bb0b7ac9dc345c8f3314ac2e
kate patch <kate.epatch@gmail.com>
2022-10-01 11:18:11 -0600

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/project-3-under-a-tack',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/Under-A-Tack',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
